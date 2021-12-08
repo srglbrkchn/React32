9d5aaaf8970c6f99103e736b91c6a539526af5cf
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-08 11:35:59 +0100

    Manage complex state of an array using useState, use map function to display content of items array.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem]=useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const inputValue = event.target.value;
+    setItem(inputValue);
+  }
+
+  function handleClick () {
+    setItems(preValue => {
+      return (
+        [...preValue,
+          item
+        ]
+      );
+    });
+    setItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input value={item} onChange={handleChange} type="text" />
+        <button onClick = {handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+
+          {
+            items.map((item, index)=> {
+              return (<li key={index}>{item}</li>);
+            })
+          }
+          
         </ul>
       </div>
     </div>
